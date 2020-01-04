815d957c804b137cdabd0a0085ca12eb9dbbc108
ESLEY W <wesley16838@gmail.com>
2020-01-04 22:19:22 +0800

    make it better

--- amazon.py
+++ amazon.py
@@ -15,6 +15,7 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+# driver = webdriver.Chrome('./chromedriver')
 driver.get(url)
 
 #find all elements with a class that ends in 'tweet-text'
