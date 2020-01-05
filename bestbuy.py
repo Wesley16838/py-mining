1d7d4cc157c04076913373f3d951b0055225efaf
ESLEY W <wesley16838@gmail.com>
2020-01-05 23:13:53 +0800

    make it better

--- bestbuy.py
+++ bestbuy.py
@@ -12,13 +12,15 @@ import os
 numset = {'0','1','2','3','4','5','6','7','8','9'}
 
 url='https://www.bestbuy.com/site/searchpage.jsp?st='
-key=sys.argv[1]+'+'+sys.argv[2]
+# key=sys.argv[1]+'+'+sys.argv[2]
+key='insignia+tv'
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
 driver.get(url+key)
 
 pageNum=1 # number of pages to collect
