0e4187f831dd84313cbb86e9723d4422cd322bb7
ESLEY W <wesley16838@gmail.com>
2020-01-10 08:50:30 -0500

    make it better

--- app.js
+++ app.js
@@ -28,9 +28,7 @@ app.get('/amazon', async (req , res) => {
 app.get('/bestbuy', async (req , res) => {
     console.log('in bestbuy mining!!!')
     kw=req.query.keyword.split(' ')
-    var process = await spawn('python', ["./bestbuy.py", kw[0] , kw[1]],{
-        timeout: 20000
-      });
+    var process = spawn('python', ["./bestbuy.py", kw[0] , kw[1]]);
     process.stdout.on('data', function (data) {
         console.log(data.toString())
     });
--- bestbuy.py
+++ bestbuy.py
@@ -17,6 +17,9 @@ key=sys.argv[1]+'+'+sys.argv[2]
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-popup-blocking")
+chrome_options.add_argument("--disable-default-apps")
+chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
@@ -24,6 +27,7 @@ driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), c
 print('bf get')
 print('key,',key)
 print('url+key,',url+key)
+driver.implicitly_wait(5)
 driver.get(url+key)
 print('af get')
 pageNum=1 # number of pages to collect
