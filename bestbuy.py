283f86ead76d943dad3f28b9de63bb8e5895abe7
ESLEY W <wesley16838@gmail.com>
2020-02-10 22:38:31 -0500

    make it better

--- bestbuy.py
+++ bestbuy.py
@@ -17,10 +17,9 @@ key=sys.argv[1]+'+'+sys.argv[2]
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-popup-blocking")
-chrome_options.add_argument("--disable-default-apps")
+
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--disable-gpu")
+
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 # driver = webdriver.Chrome('./chromedriver')
